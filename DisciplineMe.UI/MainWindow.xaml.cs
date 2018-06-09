10a56a207965a473ddf7227ee8f9e18f20213bdb
ultrafiolet13 <ultrafiolet13@gmail.com>
2018-06-09 14:44:09 +0300

    add dates highlighting

--- DisciplineMe.UI/MainWindow.xaml.cs
+++ DisciplineMe.UI/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -21,13 +22,47 @@ namespace DisciplineMe.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<DateTime> _dates = new List<DateTime>
+        {
+            new DateTime(2018,6,25),
+            new DateTime(2018,6,23),
+            new DateTime(2018,6,14)
+        };
+
         public MainWindow()
         {
-            InitializeComponent();   
-            
-            // IRepository repo = new Repository();
-            // repo.Create("Morning wake up before 7 am");
-            // var habit = repo.Read();
+            InitializeComponent();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        // Next part were taken from http://mom.hlmjr.com/2016/11/07/highlighting-dates-on-a-wpf-calendar-control/
+        // for dates highliting in the Calendar.
+
+        private void calendarButton_Loaded(object sender, EventArgs e)
+        {
+            CalendarDayButton button = (CalendarDayButton)sender;
+            DateTime date = (DateTime)button.DataContext;
+            HighlightDay(button, date);
+            button.DataContextChanged += new DependencyPropertyChangedEventHandler(calendarButton_DataContextChanged);
+        }
+
+        private void HighlightDay(CalendarDayButton button, DateTime date)
+        {
+            if (_dates.Contains(date))
+                button.Background = Brushes.LightBlue;
+            else
+                button.Background = Brushes.White;
+        }
+
+        private void calendarButton_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            CalendarDayButton button = (CalendarDayButton)sender;
+            DateTime date = (DateTime)button.DataContext;
+            HighlightDay(button, date);
         }
     }
 }
