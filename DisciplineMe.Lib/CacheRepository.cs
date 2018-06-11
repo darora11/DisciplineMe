155a93ef4c59d92cfb13a0fc11367e0d9d95ff82
dariya.arora <dariya.arora@gmail.com>
2018-06-11 07:44:11 +0300

    Add factory and storage_mode config param

---
+++ DisciplineMe.Lib/CacheRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DisciplineMe.Lib.models;
+
+namespace DisciplineMe.Lib
+{
+    class CacheRepository : IHabitRepository
+    {
+        public void Create(string Title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(string Title, string QuestionPhrase, TimeSpan ActiveDuration, TimeSpan MsgTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Habit Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DisciplineMe.Lib/RepoFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace DisciplineMe.Lib
+{
+    public class RepoFactory
+    {
+        private RepoFactory _factory;
+        public RepoFactory Factory
+        {
+            get
+            {
+                if (_factory == null)
+                    _factory = new RepoFactory();
+                return _factory;
+            }
+        }
+
+        public IHabitRepository GetHabitRepository()
+        {
+            var storageMode = ConfigurationManager.AppSettings["storage_mode"];
+            if (String.Equals(storageMode, "cache"))
+                return new CacheRepository();
+            else if (String.Equals(storageMode, "db"))
+                return new DbRepository();
+            else
+                throw new Exception("In app.config the parameter 'storage_mode' can take only two values:\n" +
+                    "'cache' - for using ICollection for managing data in the app;\n" +
+                    "'db' - for using MSSQL-Server for that purpose.");
+        }
+    }
+}
